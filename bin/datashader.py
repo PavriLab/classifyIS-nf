67779cfa9244e037add66f6c8165475fb9fd8a21
Daniel Malzl <52696584+dmalzl@users.noreply.github.com>
2020-08-06 09:11:22 +0200

    Update datashader.py

--- bin/datashader.py
+++ bin/datashader.py
@@ -233,11 +233,8 @@ def plotmesh(hs, clrmaps, xlims, ylims, datalabels, background = None, ax = None
             startpos = (ceil(min_) - min_) * step
             startlabel = ceil(min_)
         
-        print(spacer, spacing, axismax, max_, min_, integerscale)
-        print(np.arange(startpos, axismax, spacer * spacing))
-        print(list(range(startlabel, int(max_) + 1, spacing)))
         setpos(np.arange(startpos, axismax, spacer * spacing))
-        setlabels(range(startlabel, int(max_) + 1, spacing))
+        setlabels(range(startlabel, int(max_), spacing))
 
         if grid:
             ax.grid(ls = '-', zorder = 1, color = gridcolor)
